f581b5d7eab2080e5333997bb5e24bec8fae52ac
mkoltsov <root@javabean.ru>
2015-12-23 14:19:50 +0100

    progfun cource re-cap completed

--- w1/recap.scala
+++ w1/recap.scala
@@ -12,19 +12,47 @@ case class JBool(b:Boolean) extends JSON
 
 case object JNull extends JSON
 
-val data = JObj(Map(
+val data = JObj(Map( 
 	"firstName" -> JStr("John"),
 	"lastName" -> JStr("Smith"),
 	"address" -> JObj(Map(
 		"streetAddress" -> JStr("21 2nd street"),
 		"state" -> JStr("NY"),
-		"postalCode"-> JNum(10021)
-		)),
+		"postalCode"-> JNum(10021))),
 	"phoneNumbers" -> JSeq(List(
 		JObj(Map(
 			"type" -> JStr("home"),
-			"number" -> JStr("212 555-1234"),
+			"number" -> JStr("212 555-1234")
 			)),
-		"type" -> JStr("fax")))
+		JObj(Map(
+			"type" -> JStr("work"),
+			"number" -> JStr("111 555-1234")
+			))
+		))
 	))
 
+def show(json:JSON):String = json match {
+	case JSeq(elems) => s"[${(elems map show mkString ",")}]"
+	case JObj(bindings) => val assocs = bindings map {
+		case (key, value) => s"${'"'}${key}${'"'}:${show(value)}"
+	}
+	s"{${assocs mkString ","}}"
+	case JNum(num) => num.toString
+	case JStr(str) => str
+	case JBool(b ) => b.toString
+	case JNull => "null"   
+}
+
+println(show(data))
+
+val foo:String =>String  =  {case  "ping" => "pong"}
+val f:PartialFunction[String,String]  =  {case  "ping" => "pong"}
+
+println(f("ping"))
+println(f.isDefinedAt("ping"))
+println(f.isDefinedAt("abc "))
+
+val fn = (x:Int) => println(s"the number is greater that 100: ${x>100}")
+
+fn(99)
+fn(101)
