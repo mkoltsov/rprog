6d528724f6aa32fec625347829902e3350f8482e
mkoltsov <root@javabean.ru>
2016-01-14 10:11:07 +0000

    akka example started to work

--- w5/akka-actors/src/main/scala/Hello.scala
+++ w5/akka-actors/src/main/scala/Hello.scala
@@ -1,7 +1,7 @@
 import akka.actor.Actor
 
 class Counter extends Actor {
-    var count =0 
+    var count = 0 
 
     def receive = {
 	case "incr" => count+=1
@@ -11,18 +11,24 @@ class Counter extends Actor {
 
 import  akka.actor.Props
 
-object Hello extends App with Actor{
+class HelloWorld extends Actor {
 
-  override def main(args: Array[String]): Unit = {
     val counter = context.actorOf(Props[Counter], "counter")
 
-    counter ! "incr"
-    counter ! "incr"
-    counter ! "get"
+        counter ! "incr"
+        counter ! "incr"
+        counter ! "get"
+
+     def receive = {
+        case count:Int => println(s"count was $count")
+        context.stop(self)
+    }
+}
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    akka.Main.main(Array(classOf[HelloWorld].getName))
   }
 
-    def receive = {
-	case count:Int => println(s"count was $count")
-	context.stop(self)
-    }
 }
